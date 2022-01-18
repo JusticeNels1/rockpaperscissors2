4d8623ae66ec3b7f649d19ffce36011bb6f05425
JusticeNels1 <jnelsondev@gmail.com>
2022-01-18 18:23:40 -0500

    Finished

--- script.js
+++ script.js
@@ -1,37 +1,73 @@
 //create function that controls the flow of the game
-function playRound (){
-    let computerPlay = null
-    let userPlay = null
+function playRound (userPlay,computerPlay){
 
-    getPlays
+    console.log(userPlay,computerPlay)
+    if (userPlay == computerPlay){
+        console.log("tie!")
+    } else {
+        return gameLogic(userPlay,computerPlay)
+    }
+    
+}
+
+
+let getPlays = () =>  {
+    const plays = ["rock","paper","scissors"]  
+
+    let computerPlay = (() => plays[Math.floor(Math.random() * 3)])()  //create random number to pick random play from array
+    let userPlay = prompt("Please enter either rock paper or scissors"," ").toLowerCase()
+
+    while(plays.indexOf(userPlay) == -1) {userPlay = prompt("Please enter a valid play"," ")}//check if player has input a valid answer
+    
     return [userPlay,computerPlay]
 }
 
 
-let getPlays = (() =>  {
-    const plays = ["rock","paper","scissors"]  
-    computerPlay = (() => plays[Math.floor(Math.random() * 3)])()  //create random number to pick random play from array
-    userPlay = prompt("Please enter either rock paper or scissors"," ")
-    if (plays.indexOf(userPlay) == -1) {userPlay = prompt("Please enter a valid play"," ")}//check if player has input a valid answer
-    
-})()
 
-// console.log(getPlays())
-console.log(game())
 //create block of logic to determine who's play is higher
-function gameLogic (){
+function gameLogic (userPlay,computerPlay){
 
     switch (userPlay) {
         case "rock":
-            if (computerPlay === "scissors") {console.log("you win")}
+            return (computerPlay === "scissors" ? true : false);
         break;
         case "scissors":
-            if (computerPlay === "paper") {console.log("you win")}
+            return (computerPlay === "paper" ? true : false);
         break;
         case "paper":
-            if (computerPlay === "rock") {console.log("you win")}
+            return (computerPlay === "rock" ? true : false);
         break;
-        default: console.log("You Lose")
+        default: console.log("You Fuch");
         break;
     }
-}                // console.log(computerPlay)
+}          
+
+function game () {
+    let counter = 0;
+    let cpuCounter = 0;
+    for (let i = 0; i < 5; i++) {
+        let arr = getPlays();
+        let round = playRound(arr[0],arr[1])
+
+
+        if (round) {
+            console.log(`You Win ${arr[0]} beats ${arr[1]}!`);
+            counter ++;
+            console.log("counter: " + counter)
+            console.log("cpu: " + cpuCounter)
+        } else if (round != undefined){
+            console.log(`You lose ${arr[1]} beats ${arr[0]}!`)
+            cpuCounter ++
+            console.log("counter: " + counter)
+            console.log("cpu: " + cpuCounter)
+        }else {i --}
+    }
+
+    if (counter - cpuCounter > 0|| false) {
+        console.log("You Win")
+        return
+    }
+    console.log("You Lose")
+}
+
+console.log(game())
